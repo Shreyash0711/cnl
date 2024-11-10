78328959b10bd407954d3f810c12ef46aa6e04da
Shreyash Pise <125645116+Shreyash0711@users.noreply.github.com>
2024-11-10 18:25:05 +0530

    MyServer1.java

--- 6/MyServer1.java
+++ 6/MyServer1.java
@@ -13,11 +13,10 @@ String str="",str2="";
 while(!str.equals("stop")){
 str=din.readUTF();
 System.out.println("client says: "+str);
+System.out.print("Enter message: ");
 str2=br.readLine();
 dout.writeUTF(str2);
 dout.flush(); } din.close();
 s.close();
 ss.close();
-}} {
-    
-}
+}}
