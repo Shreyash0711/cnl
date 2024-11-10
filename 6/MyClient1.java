3cb1883b756c8a907fb46e32e29f7571066baa7c
Shreyash Pise <125645116+Shreyash0711@users.noreply.github.com>
2024-11-10 18:23:59 +0530

    MyClient1.java

--- 6/MyClient1.java
+++ 6/MyClient1.java
@@ -1,21 +1,33 @@
-//6a
+// 6a
 import java.net.*;
 import java.io.*;
-class MyClient1{
-public static void main(String args[])throws Exception{
-Socket s=new Socket("localhost",3333);
-DataInputStream din=new DataInputStream(s.getInputStream());
-DataOutputStream dout=new DataOutputStream(s.getOutputStream());
-BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-Example of Java Socket Programming (Read-Write both side)
 
-String str="",str2="";
-while(!str.equals("stop")){
-str=br.readLine();
-dout.writeUTF(str);
-dout.flush();
-str2=din.readUTF();
-System.out.println("Server says: "+str2);
+class MyClient1 {
+    public static void main(String args[]) throws Exception {
+        // Create a socket connection to localhost on port 3333
+        Socket s = new Socket("localhost", 3333);
+        
+        // Set up input and output streams
+        DataInputStream din = new DataInputStream(s.getInputStream());
+        DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String str = "", str2 = "";
+        
+        // Communication loop
+        while (!str.equals("stop")) {
+            System.out.print("Enter message: "); // Prompt for user input
+            str = br.readLine();
+            dout.writeUTF(str);
+            dout.flush();
+            
+            str2 = din.readUTF(); // Read response from server
+            System.out.println("Server says: " + str2);
+        }
+        
+        // Close the streams and socket
+        dout.close();
+        din.close();
+        s.close();
+    }
 }
-dout.close(); s.close();
-}}
